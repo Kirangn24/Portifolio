d407aa9a729041dbec15e78efbb2a7074d7e137b
KIRAN G N <143183067+Kirangn24@users.noreply.github.com>
2025-04-03 11:10:08 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -44,7 +44,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Listening Music
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
